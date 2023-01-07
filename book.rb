54a3138de25627d2abe78efb763b760b24788430
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-07 15:47:04 +0500

    Create rental.rb and make book and person variable writable too

--- book.rb
+++ book.rb
@@ -8,18 +8,3 @@ class Book
     @rentals = []
   end
 end
-
-class Rental
-  attr_reader :book, :person
-  attr_accessor :date
-
-  def initialize(date, book, person)
-    @date = date
-
-    @book = book
-    book.rentals << self
-
-    @person = person
-    person.rentals << self
-  end
-end
